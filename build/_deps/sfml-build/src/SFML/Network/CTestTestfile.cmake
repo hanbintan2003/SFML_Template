1bec196fa6715041012322671a594b4c46205d96
hanbintan2003 <thb182003@gmail.com>
2023-03-12 16:23:57 -0700

    fix bug

---
+++ build/_deps/sfml-build/src/SFML/Network/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/ice/Projects/MacOS_SFML_Template/build/_deps/sfml-src/src/SFML/Network
+# Build directory: /Users/ice/Projects/MacOS_SFML_Template/build/_deps/sfml-build/src/SFML/Network
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
